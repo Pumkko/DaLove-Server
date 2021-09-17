7f8598290a86e88fff1187739f137ae8cc3ee04e
Puma <c.coque@hotmail.fr>
2021-09-17 16:00:50 +0200

    Ajout du paramètre dans l'appsettings qui contiendra vers la connexion string vers le blob storage

--- DaLove-Server/Controllers/WeatherForecastController.cs
+++ DaLove-Server/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DaLove_Server.Options;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -17,23 +18,32 @@ namespace DaLove_Server.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AzureBlobOptions _azureBlobOptions;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, AzureBlobOptions azureBlobOptions)
         {
             _logger = logger;
+            _azureBlobOptions = azureBlobOptions;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult Get()
         {
+            if (string.IsNullOrEmpty(_azureBlobOptions.ConnectionString))
+            {
+                return Unauthorized("Failed");
+            }
+
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
+            return Ok(result);
         }
     }
 }
