195075cc238981706df4645511dc33996917ff11
Puma <c.coque@hotmail.fr>
2021-09-17 16:32:39 +0200

    add random video process to random video controller

--- DaLove-Server/Controllers/RandomMemoriesController.cs
+++ DaLove-Server/Controllers/RandomMemoriesController.cs
@@ -3,9 +3,6 @@ using Azure.Storage.Sas;
 using DaLove_Server.Options;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DaLove_Server.Controllers
 {
@@ -15,6 +12,29 @@ namespace DaLove_Server.Controllers
     {
         private readonly AzureBlobOptions _azureBlobOptions;
 
+        private static readonly string[] _videos = {
+            "DanielGotRaped.mp4",
+            "DanielIsHappyToSeeUs.mp4",
+            "DanielLovesKevin.mp4",
+            "DanielNeedsToTakeAShit.mp4",
+            "DanielSliding.mp4",
+            "DanielWillDie.mp4",
+            "IWantYouInMyRoom.mp4",
+            "JulienWeirdSexuality.mp4",
+            "KevinFitsInALocker.mp4",
+            "Randelo.mp4",
+            "Revenge.mp4",
+            "SexierLeon.mp4",
+            "Tattoos.mp4",
+            "TryScorpions.mp4",
+            "WomenToilets.mp4",
+            "julianCatcher.mp4",
+            "julianShakira.mp4",
+            "julianTitanic.mp4",
+            "sexyJulian.mp4"
+        };
+
+
         public RandomMemoriesController(AzureBlobOptions azureBlobOptions)
         {
             _azureBlobOptions = azureBlobOptions;
@@ -23,9 +43,15 @@ namespace DaLove_Server.Controllers
         [HttpGet]
         public ActionResult GetRandomMemories()
         {
+            var rand = new Random();
+
+            var randomIndex = rand.Next(0, _videos.Length - 1);
+
+            var videoName = _videos[randomIndex];
+
             var blobContainerClient = new BlobContainerClient(_azureBlobOptions.ConnectionString, _azureBlobOptions.MemoryContainer);
 
-            var blobClient = blobContainerClient.GetBlobClient("DanielGotRaped.mp4");
+            var blobClient = blobContainerClient.GetBlobClient(videoName);
             var sasUri = blobClient.GenerateSasUri(BlobSasPermissions.Read, DateTimeOffset.Now.AddMinutes(10));
             return Ok(sasUri.AbsoluteUri);
         }
